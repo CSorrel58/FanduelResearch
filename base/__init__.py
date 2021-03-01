c1a5feedfc1534a4e4113c4452003c4ccd16f226
CSorrel58 <clintonsorrel@gmail.com>
2021-03-01 17:55:05 -0500

    Cleaned up script
    Added logging and cleaned up script to use sub pattern instead of all lines

--- base/__init__.py
+++ base/__init__.py
@@ -1,2 +1,4 @@
 import os
+import re
+from base.utils import (add_position_nfl, clean_column)
 gdrive = os.path.join("/Users/a09304858/Documents/GitHub/FanduelResearch/base/gdrive_key.json")
--- cli/commands.py
+++ cli/commands.py
@@ -3,12 +3,28 @@
 """ Use this to execute commands to run either process. Recomemnded you use pipenv shell before executing."""
 import click
 import os
+import logging
+import logging.config
+
 from NFL.NFLFanduel import run_nfl
+
+# Adding logger.
+path = os.path.dirname(os.path.realpath(__file__))
+logging.config.fileConfig(f"cli/logging.ini", disable_existing_loggers=False)
 
 @click.command()
 @click.option('--week',default=17)
-def fanduel_nfl(week):
+@click.option(
+    "--log-level",
+    "-l",
+    default="info",
+    help="Indicate the level of logging requested. Default is WARNING",
+    type=click.Choice(["debug", "info", "warning", "error", "critical"]),
+)
+def fanduel_nfl(week, log_level):
     """Authenticate with fanduel and save cookies to the given store"""
+    logs = logging.getLevelName(log_level.upper())
+    logging.getLogger().setLevel(logs)
     run_nfl()
 
 if __name__ == '__main__':
