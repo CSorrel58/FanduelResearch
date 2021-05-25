d3ec019a4f166b8d7ba3e7889508db12026e12fd
CSorrel58 <clintonsorrel@gmail.com>
2021-05-25 18:25:37 -0400

    used pre-commit
    Used pre-commit to clean things up

--- base/utils.py
+++ base/utils.py
@@ -1,5 +1,4 @@
 import os
-import logging
 from datetime import date
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
@@ -7,29 +6,34 @@ from oauth2client.service_account import ServiceAccountCredentials
 gdrive = os.path.join("base/gdrive_key.json")
 
 
-def clean_column(df, column_name:str, character:str):
+def clean_column(df, column_name: str, character: str):
     character = character
     frame = df
     column = column_name
-    frame[column] = frame[column].str.replace(character,"",regex=True)
+    frame[column] = frame[column].str.replace(character, "", regex=True)
     return frame
 
-def google_drive_upload(df, sport: str, ):
+
+def google_drive_upload(df, sport: str):
     df = df
     sport = str(sport)
     savetime = date.today()
-    df.to_csv(f'{sport}.{savetime}.csv')
-    scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
-             "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+    df.to_csv(f"{sport}.{savetime}.csv")
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/drive",
+    ]
     # establishing credentials given to me by Google API
     credentials = ServiceAccountCredentials.from_json_keyfile_name(gdrive, scope)
     client = gspread.authorize(credentials)
     # opening the sheet I am keeping the scores on
-    if sport =="NFL":
-        spreadsheet = client.open('nfl_weekly_fanduel_scores')
+    if sport == "NFL":
+        spreadsheet = client.open("nfl_weekly_fanduel_scores")
     elif sport == "NBA":
-        spreadsheet = client.open('Player_Game_Records_2021')
+        spreadsheet = client.open("Player_Game_Records_2021")
     # Updating sheet with my new csv
-    with open(f'{sport}.{savetime}.csv', 'r') as file_obj:
+    with open(f"{sport}.{savetime}.csv", "r") as file_obj:
         content = file_obj.read()
         client.import_csv(spreadsheet.id, data=content)
--- cli/commands.py
+++ cli/commands.py
@@ -20,6 +20,7 @@ from NBA.NBAFanduel import run_nba
 path = os.path.dirname(os.path.realpath(__file__))
 logging.config.fileConfig(f"cli/logging.ini", disable_existing_loggers=False)
 
+
 @click.group()
 @click.option(
     "--log-level",
@@ -32,25 +33,26 @@ def scraper(log_level):
     logs = logging.getLevelName(log_level.upper())
     logging.getLogger().setLevel(logs)
 
-@scraper.command("fanduel-nfl")
-@click.option('--week',default=19)
 
+@scraper.command("fanduel-nfl")
+@click.option("--week", default=19)
 def fanduel_nfl(week):
     """Go to Rotoguru for NFL from opening week through week number listed and scrape player data to a google sheet"""
     week = week
     run_nfl(week)
 
+
 @scraper.command("fanduel-nba")
-@click.option('--day',default=31)
-@click.option('--month',default=12)
-@click.option('--year',default=2021)
-
-def fanduel_nba(day,month,year):
+@click.option("--day", default=31)
+@click.option("--month", default=12)
+@click.option("--year", default=2021)
+def fanduel_nba(day, month, year):
     """Go to RotoGuru for NBA from opening day through day listed and scrape player data to a given sheet"""
-    day=day
-    month=month
-    year=year
-    run_nba(day,month,year)
+    day = day
+    month = month
+    year = year
+    run_nba(day, month, year)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     scraper()
